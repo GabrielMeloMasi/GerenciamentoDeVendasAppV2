54faa1ebd3aebcc161dc078de8643d5ae0d93c46
Gabriel Masi <2568455@tcs.com>
2024-11-25 21:04:36 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VendasApp',
+  webDir: 'www'
+};
+
+export default config;
